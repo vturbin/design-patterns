a9c55b096638ccc94654073c9a780e2605273623
val <v.turbin@hotmail.com>
2023-01-14 12:41:48 +0100

    adapter pattern

---
+++ Adapter/Adapter.js
@@ -0,0 +1,27 @@
+"use strict";
+// =============== Adapter Pattern ===============
+class IPhoneXs {
+    useLightning() {
+        console.log("Using lightning port..");
+    }
+}
+class GooglePixel3 {
+    useUSBc() {
+        console.log("Using USB-C port..");
+    }
+}
+// -------------------- Adapter class --------------------
+class LightningToMicroUSBAdapter {
+    constructor(iphone) {
+        this.iphoneDevice = iphone;
+    }
+    useUSBc() {
+        console.log('Want to use USBc, converting...');
+        this.iphoneDevice.useLightning();
+    }
+}
+// --------------------------------------------------------
+let iphone = new IPhoneXs();
+iphone.useLightning();
+let chargeAdaptor = new LightningToMicroUSBAdapter(iphone);
+chargeAdaptor.useUSBc();
---
+++ Adapter/Adapter.ts
@@ -0,0 +1,45 @@
+
+// =============== Adapter Pattern ===============
+
+interface IPhone {
+  useLightning():void;
+}
+
+interface Android {
+  useUSBc():void;
+}
+
+class IPhoneXs implements IPhone {
+  useLightning() {
+    console.log("Using lightning port..");
+  }
+}
+
+class GooglePixel3 implements Android {
+  useUSBc() {
+    console.log("Using USB-C port..");
+  }
+}
+
+// -------------------- Adapter class --------------------
+
+class LightningToMicroUSBAdapter implements Android {
+  iphoneDevice: IPhone;
+
+  constructor(iphone: IPhone) {
+    this.iphoneDevice = iphone;
+  }
+
+  useUSBc() {
+    console.log('Want to use USBc, converting...');
+    this.iphoneDevice.useLightning();
+  }
+}
+
+// --------------------------------------------------------
+
+let iphone = new IPhoneXs();
+iphone.useLightning();
+let chargeAdaptor = new LightningToMicroUSBAdapter(iphone);
+
+chargeAdaptor.useUSBc();
